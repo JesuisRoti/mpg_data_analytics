ed5b653f6b6331a2b74a0e5b3ed64f5c9a074865
romain <romain.bouyx@gmail.com>
2023-11-24 20:04:04 +0100

    Format

--- flask/routes.py
+++ flask/routes.py
@@ -2,12 +2,12 @@ from flask import Blueprint, request, abort, g
 import pandas as pd
 
 from general_functions import (
-    base_request,
-    prettify_json,
-    top_forward,
-    top_backward,
-    top_goal_keeper,
-    top_midfielder,
+	base_request,
+	prettify_json,
+	top_forward,
+	top_backward,
+	top_goal_keeper,
+	top_midfielder,
 )
 
 main_routes = Blueprint("main", __name__)
@@ -15,86 +15,97 @@ main_routes = Blueprint("main", __name__)
 
 @main_routes.route("/")
 def main():
-    return "True"
+	return "True"
 
 
 @main_routes.route("/top_forward")
 def r_top_forward():
-    args_dict = g.get("args_dict", {})
+	args_dict = g.get("args_dict", {})
 
-    try:
-        top_number = int(args_dict.get("top_number"))
-    except:
-        abort(403, "top_number arg must be defined in the request an be an integer")
-    else:
-        clean_df = base_request(
-            int(top_number), args_dict.get("ranking_criteria", "averagePoints")
-        )
-        result = top_forward(clean_df)
-        return prettify_json(result)
+	try:
+		top_number = int(args_dict.get("top_number"))
+	except:
+		abort(403, "top_number arg must be defined in the request an be an integer")
+	else:
+		clean_df = base_request(
+			int(top_number), args_dict.get("ranking_criteria", "averagePoints")
+		)
+		result = top_forward(clean_df)
+		return prettify_json(result)
 
 
 @main_routes.route("/top_backward")
 def r_top_backward():
-    args_dict = g.get("args_dict", {})
+	args_dict = g.get("args_dict", {})
 
-    try:
-        top_number = int(args_dict.get("top_number"))
-    except:
-        abort(403, "top_number arg must be defined in the request an be an integer")
-    else:
-        clean_df = base_request(
-            int(top_number), args_dict.get("ranking_criteria", "averagePoints")
-        )
-        result = top_backward(clean_df)
-        return prettify_json(result)
+	try:
+		top_number = int(args_dict.get("top_number"))
+	except:
+		abort(403, "top_number arg must be defined in the request an be an integer")
+	else:
+		clean_df = base_request(
+			int(top_number), args_dict.get("ranking_criteria", "averagePoints")
+		)
+		result = top_backward(clean_df)
+		return prettify_json(result)
 
 
 @main_routes.route("/top_goal")
 def r_top_goal():
-    args_dict = g.get("args_dict", {})
+	args_dict = g.get("args_dict", {})
 
-    try:
-        top_number = int(request.args.get("top_number"))
-    except:
-        abort(403, "top_number arg must be defined in the request an be an integer")
-    else:
-        clean_df = base_request(
-            int(top_number), args_dict.get("ranking_criteria", "averagePoints")
-        )
-        result = top_goal_keeper(clean_df)
-        return prettify_json(result)
+	try:
+		top_number = int(request.args.get("top_number"))
+	except:
+		abort(403, "top_number arg must be defined in the request an be an integer")
+	else:
+		clean_df = base_request(
+			int(top_number), args_dict.get("ranking_criteria", "averagePoints")
+		)
+		result = top_goal_keeper(clean_df)
+		return prettify_json(result)
 
 
 @main_routes.route("/top_midfielder")
 def r_top_midfielder():
-    args_dict = g.get("args_dict", {})
+	args_dict = g.get("args_dict", {})
 
-    try:
-        top_number = int(request.args.get("top_number"))
-    except:
-        abort(403, "top_number arg must be defined in the request an be an integer")
-    else:
-        clean_df = base_request(
-            int(top_number), args_dict.get("ranking_criteria", "averagePoints")
-        )
-        result = top_midfielder(clean_df)
-        return prettify_json(result)
+	try:
+		top_number = int(request.args.get("top_number"))
+	except:
+		abort(403, "top_number arg must be defined in the request an be an integer")
+	else:
+		clean_df = base_request(
+			int(top_number), args_dict.get("ranking_criteria", "averagePoints")
+		)
+		result = top_midfielder(clean_df)
+		return prettify_json(result)
 
 
 @main_routes.route("/top_players")
 def r_top_players():
-    args_dict = g.get("args_dict", {})
-
-    try:
-        top_number = int(request.args.get("top_number"))
-    except:
-        abort(403, "top_number arg must be defined in the request an be an integer")
-    else:
-        clean_df = base_request(
-            int(top_number), args_dict.get("ranking_criteria", "averagePoints")
-        )
-        position_wanted = []
-
-        result = pd.concat([top_forward(clean_df), top_midfielder(clean_df)])
-        return prettify_json(result)
+	args_dict = g.get("args_dict", {})
+	try:
+		top_number = int(request.args.get("top_number"))
+	except:
+		abort(403, "top_number arg must be defined in the request an be an integer")
+	else:
+		clean_df = base_request(
+			int(top_number), args_dict.get("ranking_criteria", "averagePoints")
+		)
+		position_wanted = []
+		for position in args_dict.get("position").split(","):
+			match position:
+				case "A":
+					position_wanted.append(top_forward(clean_df))
+				case "D":
+					position_wanted.append(top_backward(clean_df))
+				case "M":
+					position_wanted.append(top_midfielder(clean_df))
+				case "G":
+					position_wanted.append(top_goal_keeper(clean_df))
+		if position_wanted:
+			result = pd.concat(position_wanted)
+		else:
+			result = top_forward(clean_df)
+		return prettify_json(result)
--- mpg_main.py
+++ mpg_main.py
@@ -6,130 +6,129 @@ import requests
 from sqlalchemy import create_engine
 
 from general_functions import (
-    cleaning,
-    top_pos,
-    top_forward,
-    top_backward,
-    top_goal_keeper,
-    top_midfielder,
-    add_extra_info,
-    clean_extra_data,
+	cleaning,
+	top_pos,
+	top_forward,
+	top_backward,
+	top_goal_keeper,
+	top_midfielder,
+	add_extra_info,
+	clean_extra_data,
 )
 
 
 def plot_players(
-    colors: np.array,
-    player_dataframe: pd.DataFrame,
-    x_col: str,
-    y_col: str,
-    x_step: float,
-    y_step: float,
-    x_leg: str,
-    y_leg: str,
-    title: str,
+		colors: np.array,
+		player_dataframe: pd.DataFrame,
+		x_col: str,
+		y_col: str,
+		x_step: float,
+		y_step: float,
+		x_leg: str,
+		y_leg: str,
+		title: str,
 ):
-    fig, ax = plt.subplots()
+	fig, ax = plt.subplots()
 
-    plt.yticks(
-        np.arange(min(player_dataframe[y_col]), max(player_dataframe[y_col]), y_step)
-    )
-    plt.xticks(
-        np.arange(min(player_dataframe[x_col]), max(player_dataframe[x_col]), x_step)
-    )
+	plt.yticks(
+		np.arange(min(player_dataframe[y_col]), max(player_dataframe[y_col]), y_step)
+	)
+	plt.xticks(
+		np.arange(min(player_dataframe[x_col]), max(player_dataframe[x_col]), x_step)
+	)
 
-    plt.grid(True)
-    plt.title(title)
-    ax.scatter(player_dataframe[x_col], player_dataframe[y_col], c=colors)
+	plt.grid(True)
+	plt.title(title)
+	ax.scatter(player_dataframe[x_col], player_dataframe[y_col], c=colors)
 
-    legend_labels = []
-    for j, name in enumerate(player_dataframe["playerFullName"]):
-        legend_labels.append(ax.scatter([], [], c=colors[j], label=name))
+	legend_labels = []
+	for j, name in enumerate(player_dataframe["playerFullName"]):
+		legend_labels.append(ax.scatter([], [], c=colors[j], label=name))
 
-    ax.legend(handles=legend_labels, bbox_to_anchor=(1, 1))
+	ax.legend(handles=legend_labels, bbox_to_anchor=(1, 1))
 
-    ax.set_xlabel(x_leg)
-    ax.set_ylabel(y_leg)
+	ax.set_xlabel(x_leg)
+	ax.set_ylabel(y_leg)
 
-    fig.tight_layout()
-    fig.show()
+	fig.tight_layout()
+	fig.show()
 
 
 if __name__ == "__main__":
-    res = requests.get(
-        "https://api.mpg.football/api/data/championship-players-pool/2?season=2023",
-        verify=False,
-    )
-    jdata = res.json()
+	res = requests.get(
+		"https://api.mpg.football/api/data/championship-players-pool/2?season=2023",
+		verify=False,
+	)
+	jdata = res.json()
 
-    pd.set_option("float_format", "{:.2f}".format)
+	pd.set_option("float_format", "{:.2f}".format)
 
-    df = pd.DataFrame(player for player in jdata["poolPlayers"])
-    engine = create_engine("postgresql://postgres:root@localhost:5432/test_mpg")
-    clean_df = cleaning(df)
-    top_numb = 10
-    top_pos = top_pos(clean_df, top_numb)
-    best_forward = top_forward(top_pos)
-    best_back = top_backward(top_pos)
-    best_middle = top_midfielder(top_pos)
-    best_goalk = top_goal_keeper(top_pos)
+	df = pd.DataFrame(player for player in jdata["poolPlayers"])
+	engine = create_engine("postgresql://postgres:root@localhost:5432/test_mpg")
+	clean_df = cleaning(df)
+	top_numb = 10
+	top_pos = top_pos(clean_df, top_numb)
+	best_forward = top_forward(top_pos)
+	best_back = top_backward(top_pos)
+	best_middle = top_midfielder(top_pos)
+	best_goalk = top_goal_keeper(top_pos)
 
-    extra_stats_bf = add_extra_info(best_forward)
-    best_forward = clean_extra_data(extra_stats_bf, best_forward)
+extra_stats_bf = add_extra_info(best_forward)
+best_forward = clean_extra_data(extra_stats_bf, best_forward)
 
-    colors_list = np.array(
-        [
-            "black",
-            "red",
-            "blue",
-            "green",
-            "skyblue",
-            "lime",
-            "pink",
-            "orange",
-            "maroon",
-            "gray",
-        ]
-    )
-    titles = ["Goal Keeper", "Mid Fielder", "Backward", "Forward"]
-    granularity = [
-        {
-            "title": "Goal Keeper",
-            "dataframe": best_goalk,
-            "y_step": 0.1,
-            "x_step": 2,
-        },
-        {
-            "title": "Mid Fielder",
-            "dataframe": best_middle,
-            "y_step": 0.1,
-            "x_step": 5,
-        },
-        {
-            "title": "Backward",
-            "dataframe": best_back,
-            "y_step": 0.1,
-            "x_step": 5,
-        },
-        {
-            "title": "Forward",
-            "dataframe": best_forward,
-            "y_step": 0.1,
-            "x_step": 5,
-        },
-    ]
+colors_list = np.array(
+	[
+		"black",
+		"red",
+		"blue",
+		"green",
+		"skyblue",
+		"lime",
+		"pink",
+		"orange",
+		"maroon",
+		"gray",
+	]
+)
+titles = ["Goal Keeper", "Mid Fielder", "Backward", "Forward"]
+granularity = [
+	{
+		"title": "Goal Keeper",
+		"dataframe": best_goalk,
+		"y_step": 0.1,
+		"x_step": 2,
+	},
+	{
+		"title": "Mid Fielder",
+		"dataframe": best_middle,
+		"y_step": 0.1,
+		"x_step": 5,
+	},
+	{
+		"title": "Backward",
+		"dataframe": best_back,
+		"y_step": 0.1,
+		"x_step": 5,
+	},
+	{
+		"title": "Forward",
+		"dataframe": best_forward,
+		"y_step": 0.1,
+		"x_step": 5,
+	},
+]
 
-    for i in granularity:
-        plot_players(
-            colors=colors_list,
-            player_dataframe=i["dataframe"],
-            x_col="quotation",
-            y_col="averageRating",
-            x_step=i["x_step"],
-            y_step=i["y_step"],
-            x_leg="Quotation",
-            y_leg="Rating",
-            title=i["title"],
-        )
-
+for i in granularity:
+	plot_players(
+		colors=colors_list,
+		player_dataframe=i["dataframe"],
+		x_col="quotation",
+		y_col="averageRating",
+		x_step=i["x_step"],
+		y_step=i["y_step"],
+		x_leg="Quotation",
+		y_leg="Rating",
+		title=i["title"],
+	)
 
 # df.to_sql('table_name', engine, if_exists='append')
